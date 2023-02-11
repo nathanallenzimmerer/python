f9c058362b6fe8c6e8f911c34626802c5e1359a2
Nathan Allen-Zimmerer <38819189+nathanallenzimmerer@users.noreply.github.com>
2023-02-10 19:34:01 -0800

    Add files via upload

---
+++ Detemine whether number is even or odd.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even number". format(num))
+else:
+    print("{0} is Odd number". format(num))
